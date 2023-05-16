31dede1dbf4bc870c3b6ac033a3dcc76a9937829
Bilal Alsilwadi <balsilwadi@gmail.com>
2023-05-15 23:00:09 -0500

    dom practice

--- assignment.js
+++ assignment.js
@@ -1,8 +1,19 @@
 /* Task 1 */
 // ... your solution
+const task1 = document.querySelector('#task-1');
+// task1.style.background = 'black';
+// task1.style.color = 'white';
+task1.setAttribute("style", "background: black; color:white;")
+
 
 /* Task 2 */
 // ... your solution
 
+//document.title = 'Assignment - Finished!';
+document.querySelector('title').textContent = 'Assignment - Finished!';
+
+
 /* Task 3 */
 // ... your solution
+
+document.querySelector('h1').textContent = 'Assignment - Finished!';
